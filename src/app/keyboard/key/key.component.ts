7d285caf67cfc1ab97c1afaf33302a67563fae7b
IntegerVector <andrii.ruban@outlook.com>
2021-06-28 15:16:47 +0300

    parameter inkey component renamed

--- src/app/keyboard/key/key.component.ts
+++ src/app/keyboard/key/key.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class KeyComponent {
   @Input()
-  key!: string;
+  code!: string;
   @Input()
   highlighted!: boolean;
   @Input()
