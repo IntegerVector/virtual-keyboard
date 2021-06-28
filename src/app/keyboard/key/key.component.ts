187897c949056196508abc7a034127c37572a3aa
IntegerVector <andrii.ruban@outlook.com>
2021-06-28 14:49:53 +0300

    key component added

---
+++ src/app/keyboard/key/key.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-key',
+  templateUrl: './key.component.html',
+  styleUrls: ['./key.component.css']
+})
+export class KeyComponent {
+  @Input()
+  key!: string;
+  @Input()
+  highlighted!: boolean;
+  @Input()
+  enabled!: boolean;
+  @Input()
+  active!: boolean;
+  @Input()
+  label!: string;
+  @Input()
+  shiftLabel!: string;
+  @Input()
+  altLabel!: string;
+
+  @Output() clicked = new EventEmitter<string>();
+
+  constructor() { }
+
+  public getClasses(): string {
+    const isHighlighted = this.highlighted ? 'key-container--highlighted' : '';
+    const isEnabled = this.enabled ? 'key-container--clickable' : '';
+    const isActive = this.active ? 'key-container--activated' : '';
+
+    return `${isHighlighted} ${isEnabled} ${isActive}`;
+  }
+}
---
+++ src/app/keyboard/keyboard.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KeyComponent } from './key/key.component';
+
+
+
+@NgModule({
+  declarations: [
+    KeyComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    KeyComponent
+  ]
+})
+export class KeyboardModule { }
