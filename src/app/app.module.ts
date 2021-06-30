81e49d461d7bcadd329946ad174b8b629cbc1d35
IntegerVector <andrii.ruban@outlook.com>
2021-06-30 20:05:19 +0300

    mock local storage service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { UiElementsModule } from 'src/app/ui-elements/ui-elements.module';
 import { KeyboardModule } from './keyboard/keyboard.module';
+import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
 import { DataSourceService } from './services/data-source/data-source.service';
 import { AppComponent } from './app.component';
 
@@ -13,9 +15,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    UiElementsModule,
     KeyboardModule
   ],
-  providers: [DataSourceService],
+  providers: [
+    DataSourceService,
+    LocalStorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
