e3ac7c7466c9072649da61a732bf8b3d59d3684c
IntegerVector <andrii.ruban@outlook.com>
2022-04-25 13:22:04 +0300

    DataSourceService updated for new angular

--- src/app/services/data-source/data-source.service.ts
+++ src/app/services/data-source/data-source.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 import { AvailableLayout } from 'src/app/services/data-source/types/available-layout.interface';
 import { LayoutData } from './types/layout-data.interface';
@@ -12,23 +13,23 @@ export class DataSourceService {
   constructor(private http: HttpClient) { }
 
   public getAvailableLayouts(): Promise<AvailableLayout[]> {
-    return this
+    return lastValueFrom(this
       .http
       .get<AvailableLayout[]>('./assets/data/available-layouts.json')
-      .toPromise();
+    );
   }
 
   public getLayout(layoutCode: string): Promise<LayoutData> {
-    return this
+    return lastValueFrom(this
       .http
       .get<LayoutData>(`./assets/data/layouts/${layoutCode}.json`)
-      .toPromise();
+    );
   }
 
   public getMiniLayout(layoutCode: string): Promise<LayoutData> {
-    return this
+    return lastValueFrom(this
       .http
       .get<LayoutData>(`./assets/data/layouts/${layoutCode}-mini.json`)
-      .toPromise();
+    );
   }
 }
